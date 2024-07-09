9ef8a9fa2f15b269a5c947c104982e7cad615614
Kelvin364 <k.rwihimba@alustudents.com>
2024-07-09 11:31:15 +0200

    my first commit

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    val = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            val += 1
+            for x in range(val):
+                print("",end="")
+    except IndexError:
+        pass
+    print()
+    return val
+
+
