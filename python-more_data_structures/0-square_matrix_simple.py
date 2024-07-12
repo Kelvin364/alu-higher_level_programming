8bf67b42ba0e5f38481f33cde0de7a9db5a5a948
Kelvin364 <k.rwihimba@alustudents.com>
2024-07-12 10:24:46 +0200

    commit workload

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
 
+def square_matrix_simple(matrix=[]):
+    """
+    wordA function that computes the square
+    value of all integers of a matrix.
+    """
+    new_matrix = []#!/usr/bin/python3
+
+
 def print_sorted_dictionary(a_dictionary):
     """
     A function that prints a dictionary by ordered keys
@@ -9,3 +17,7 @@ def print_sorted_dictionary(a_dictionary):
     keys.sort()
     for key in keys:
         print("{}: {}".format(key, a_dictionary[key]))
+    for col in matrix:
+        result = list(map(lambda x: x**2, col))
+        new_matrix.append(result)
+    return new_matrix
