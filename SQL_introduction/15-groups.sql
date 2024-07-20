95d8fb5aad945081cbbcbc818c03ec5072d35d3b
kelvin364 <k.rwihimba@alustudent.com>
2024-07-20 13:17:01 +0200

    commit

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,6 +1,5 @@
--- this script counts duplicats
+-- lists the number of records with the same score in the table
 SELECT score, COUNT(*) AS number
 FROM second_table
-GROUP BY score 
-HAVING number > 1
-ORDER BY number DESC;
+GROUP BY score
+ORDER BY score DESC;
