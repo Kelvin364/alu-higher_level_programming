a10ab2c394ade3415f1d200322f17bbdcf60119c
kelvin364 <k.rwihimba@alustudent.com>
2024-07-20 13:24:02 +0200

    commit

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,8 +1,2 @@
--- list specific scoreass
-SELECT name, score FROM second_table WHERE score >= 10 ORDER BY score DESC;
--- a script that lists all records with a score >= 10
--- in the table second_table of the database
-SELECT score, name
-FROM second_table
-WHERE score >= 10
-ORDER BY score DESC;
+--  lists all records with a score >= 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
