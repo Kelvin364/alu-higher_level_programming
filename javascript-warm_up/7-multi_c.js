e89275d263741c458d7f6da7287c1500bd3a2c98
kelvin364 <k.rwihimba@alustudent.com>
2024-09-25 11:28:56 +0200

    commit

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const myarg = parseInt(process.argv[2]);
 if (isNaN(myarg)) {
-    console.log('Missing number of occurrences');
-  } else {
-    for ( let i=0 ; i<myarg; i++ )
-    console.log('C is fun');
-  }
-
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < myarg; i++) { console.log('C is fun'); }
+}
