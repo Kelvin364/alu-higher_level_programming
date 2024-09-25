bc39534d0f7ef1ced3f3629338855534bd378396
kelvin364 <k.rwihimba@alustudent.com>
2024-09-25 11:54:40 +0200

    commit

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,6 +3,6 @@ const myarg = parseInt(process.argv[2]);
 if (isNaN(myarg)) {
   console.log('Missing size');
 } else {
-   for (let j =0 ; j < mya; j++)
+   for (let j =0 ; j < myarg; j++)
        {console.log(j *('*'));}
 }
