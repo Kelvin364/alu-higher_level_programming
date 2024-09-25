b5f6dd970a1ed2c5ae87a644fcfeda546a465eb4
kelvin364 <k.rwihimba@alustudent.com>
2024-09-25 09:43:59 +0200

    commit

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-let myarg = process.argv.length -2;
-if (myarg === 1){
-    console.log('Argument found')
+const myarg = process.argv.length - 2;
+if (myarg === 1) {
+  console.log('Argument found');
+} else if (myarg > 0) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
 }
-else if (myarg > 0) {
-    console.log('Arguments found')
-}
-else
-{
- console.log('No argument')
-}
-
-
