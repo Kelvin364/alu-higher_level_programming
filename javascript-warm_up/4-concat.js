5708684e593295724bdd1237131de37b8f70f335
kelvin364 <k.rwihimba@alustudent.com>
2024-09-25 10:29:00 +0200

    commit

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const myarg1 = process.argv[2];
 const myarg2 = process.argv[3];
-if (myarg1 === undefined) {
-  console.log('No argument');
-} else {
+
   console.log(myarg1 +' ' + 'is' + ' '+ myarg2);
-}
+
