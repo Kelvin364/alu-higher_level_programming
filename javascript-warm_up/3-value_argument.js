e2df90bfd4b16939b09846d01b1b345df3213dff
kelvin364 <k.rwihimba@alustudent.com>
2024-09-25 10:11:44 +0200

    commit

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-let myarg = process.argv[2] ;
-if (myarg === undefined)
-{
-    console.log('No argument');
-}
-else
-{
+const myarg = process.argv[2];
+if (myarg === undefined) {
+  console.log('No argument');
+} else {
   console.log(myarg);
 }
