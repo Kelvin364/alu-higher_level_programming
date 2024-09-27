8bdbbb3dcbe1799eeb2c969f54cb6d8cb3e3a1ec
kelvin364 <k.rwihimba@alustudent.com>
2024-09-27 21:39:17 +0200

    commit

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -3,13 +3,13 @@ const myarg1 = parseInt(process.argv[2]);
 
 function factorial (n) {
     if (n === 0 || n === 1 ){
-        fac = 1
+        let fac = 1
         console.log(fac)
     }
     
     else {
         for ( i = n ; i > 0 ; i++){
-            fac *= (n*i);
+            lfac *= (n*i);
             console.log(fac);
         }
     }
