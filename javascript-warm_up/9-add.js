2eafe99605436d0d03ba210fa82d484d6267c143
kelvin364 <k.rwihimba@alustudent.com>
2024-09-27 18:14:55 +0200

    commit

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,9 +2,9 @@
 const myarg1 = parseInt(process.argv[2]);
 const myarg2 = parseInt(process.argv[3]);
 
-function add (myarg1,myarg2){
-let sum = (myarg1 +myarg2);
+function add (a,b){
+let sum = a + b;
 console.log(sum);
-return sum;
 }
+add(myarg1,myarg2)
 
