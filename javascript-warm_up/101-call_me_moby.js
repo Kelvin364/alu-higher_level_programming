bf753c50a4693e79353ef242acc6ea4f72fae72a
kelvin364 <k.rwihimba@alustudent.com>
2024-10-02 17:22:45 +0200

    commit

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-exports.callMeMoby = function(n,mob){
-    for ( i=0; i<n ; i++)
-    {
-        mob();
-    }
-}
+exports.callMeMoby = function (n, mob) {
+  for (i = 0; i < n; i++) {
+    mob();
+  }
+};
